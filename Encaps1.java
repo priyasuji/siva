54ecd71ed168d6d3120c4c9a2cfeec349a0f4b57
priyasuji <sivapriya183@gmail.com>
2016-08-30 08:02:01 +0530

    Create Encaps1.java

---
+++ Encaps1.java
@@ -0,0 +1,24 @@
+# sivapackage sp;
+
+public class Encaps1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		System.out.println("main method");
+		disp1();
+		disp2();
+		disp3();
+	}
+ public static void disp1()
+ {
+	 System.out.println("public");
+ }
+ private static void disp2()
+ {
+	 System.out.println("private");
+ }
+ protected static void disp3()
+ {
+	 System.out.println("protected");
+ }
+}
